d0f31c25993370be07b6671ee57ef665044a6d9f
ex0byte <dhairyawankumartiwari@gmail.com>
2025-06-23 18:57:33 +0530

    Added card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import {Card} from '@/components/Card';
 import '@/components/Theme-toggle'
 import { ModeToggle } from '@/components/Theme-toggle';
 
 export default function Home() {
   return (
-    <div >
-      <h1>AWAKEN  AI</h1>
+    <div>
+      <h1>Awaken AI</h1>
     </div>
   );
 }
